31c4ec3d336e3526af3fc33e644dd089b4a65e26
TheCatSaber <66741113+TheCatSaber@users.noreply.github.com>
2020-08-31 06:58:02 +0100

    Update to python 3
    fixes #1

--- Weierstrass.py
+++ Weierstrass.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 M = 100000
 
 def weierstrass(x, N):
-	y = np.zeros((1,M))
-	for n in xrange(1,N):
+	y = np.zeros((1 ,M))
+	for n in range(1, N):
 		y = y + np.cos(3**n*np.pi*x)/2**n
 	return y
 
-x = np.linspace(-2,2,M)
-y = np.reshape(weierstrass(x,500),(M,))
+x = np.linspace(-2, 2, M)
+y = np.reshape(weierstrass(x, 500),(M,))
 
 plt.plot(x, y, 'r-')
 plt.show()
